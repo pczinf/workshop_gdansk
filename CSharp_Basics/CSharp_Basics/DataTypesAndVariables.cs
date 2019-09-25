ceddd972b21ab3d4b9ffd270aeb373f7cde65819
Pawel <pczesnik@infoprojekt.pl>
2019-09-25 12:29:52 +0200

    operacja na intach i doublach

--- CSharp_Basics/CSharp_Basics/DataTypesAndVariables.cs
+++ CSharp_Basics/CSharp_Basics/DataTypesAndVariables.cs
@@ -56,27 +56,30 @@ namespace CSharp_Basics
         //        /// make integer variables: subtract, multiply and divide perform operations.
         //        /// discuss why divide is not working
         //        /// </summary>
-        //        [Test]
-        //        public void Operation_On_Integers()
-        //        {
-        //            int X = 4;
-        //            int Y = 5;
-        //
-        //            /*
-        //             * There are few other ways you can do this
-        //             * int add = 0;
-        //             * add = x+y;
-        //             */
-        //            int add = X + Y;
-        //
-        //            Assert.AreEqual(9, add, "Example Has wrong value did you changed it?");
-        //            Assert.AreEqual(-1, subtract, "Wrong value maybe other way around?");
-        //            Assert.That(subtract, Is.TypeOf<int>(), "You sure it is int?");
-        //            Assert.AreEqual(20, multiply, "Example Has wrong value did you changed it?");
-        //            Assert.That(multiply, Is.TypeOf<int>(), "You sure it is int?");
-        //            Assert.AreEqual(1.25, divide, "Example Has wrong value did you changed it?");
-        //            Assert.That(divide, Is.TypeOf<int>(), "You sure it is int?");
-        //        }
+        [Test]
+        public void Operation_On_Integers()
+        {
+            int X = 4;
+            int Y = 5;
+
+            /*
+             * There are few other ways you can do this
+             * int add = 0;
+             * add = x+y;
+             */
+            int add = X + Y;
+            int subtract = X - Y;
+            int multiply = X * Y;
+            double divide =  (double)Y/X;
+
+            Assert.AreEqual(9, add, "Example Has wrong value did you changed it?");
+            Assert.AreEqual(-1, subtract, "Wrong value maybe other way around?");
+            Assert.That(subtract, Is.TypeOf<int>(), "You sure it is int?");
+            Assert.AreEqual(20, multiply, "Example Has wrong value did you changed it?");
+            Assert.That(multiply, Is.TypeOf<int>(), "You sure it is int?");
+            Assert.AreEqual(1.25, divide, "Example Has wrong value did you changed it?");
+            Assert.That(divide, Is.TypeOf<double>(), "You sure it is int?");
+        }
 
         //        /// <summary>
         //        /// There are more way of changing value if you want to only increase x you can
