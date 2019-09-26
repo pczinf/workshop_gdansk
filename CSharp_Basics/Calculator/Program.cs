66d78f6e2de7bde2211e8fea1a705d0c3c91c222
Pawel <pczesnik@infoprojekt.pl>
2019-09-26 12:44:34 +0200

    Dodana klasa interfejsu UI1

--- CSharp_Basics/Calculator/Program.cs
+++ CSharp_Basics/Calculator/Program.cs
@@ -11,11 +11,6 @@ namespace Calculator
         static bool quit = false;
         public static void Main(string[] args)
         {
-            /* double x = 14;
-             double y = 230;
-             double z = 30;
-             double result = x * y;
-             */
             double result;
             int i = 0;
             /*while (i<10)
@@ -29,101 +24,15 @@ namespace Calculator
             { }*/
             while (!quit)
                 {
-                    double pnum1 = GetValueFromUser("First number=");
-                    double pnum2 = GetValueFromUser("Second number=");
-                ////result = pnum1 * pnum2;
-                ////Console.WriteLine("x*y="+result);
-                ////Console.WriteLine($"x*y={result}");
-                /*
-                result = Add(pnum1, pnum2);
-                Console.WriteLine($"x+y={result}");
-                result = Subtr(pnum1, pnum2);
-                Console.WriteLine($"x-y={result}");
-                result = Mult(pnum1, pnum2);
-                Console.WriteLine($"x*y={result}");
-                result = Div(pnum1, pnum2);
-                Console.WriteLine($"x/y={result}");
-                */
-                Console.Write("Operation: ");
-                    string operation = Console.ReadLine();
-                    if (operation == "q")
-                    {
-                        Console.WriteLine("KONIEC");
-                        Console.ReadKey();
-                        return;
-                    }
+                    double pnum1 = UI1.GetValueFromUser("First number=");
+                    double pnum2 = UI1.GetValueFromUser("Second number=");
+                    string operation = UI1.GetOperationFromUser();
                     result = Calculations.Calculation(operation, pnum1, pnum2);
                     Console.WriteLine($"Wynik={result}");
                     Console.ReadKey();
                 }
         }
 
-        public static double GetValueFromUser(string message)
-        {
-            double pnum1;
-            while (true)
-            {
-                Console.Write(message);
-                string valueFromUser = Console.ReadLine();
-                if (valueFromUser=="q")
-                {   Console.WriteLine("KONIEC");
-                    Console.ReadKey();
-                    Environment.Exit(0);
-                }
-                if (!double.TryParse(valueFromUser, out pnum1))
-                { ///Console.WriteLine("Nieliczba! Popraw!");
-                    message = "Nieliczba! Popraw! ";
-                }
-                else return pnum1;
-            }
-        }
-
-/*        public static double Calculation(string oper, double pnum1, double pnum2)
-        {
-            switch (oper)
-            {
-                case "+":
-                    return Add(pnum1, pnum2);
-                case "-":
-                    return Subtr(pnum1, pnum2);
-                case "*":
-                case "x":
-                    return Mult(pnum1, pnum2);
-                case "/":
-                    if (pnum2 == 0)
-                    {
-                        Console.WriteLine("Dzielenie przez zero!");
-                        return 0;
-                    }
-                    return Div(pnum1, pnum2);
-                default:
-                    Console.WriteLine("Nieznana operacja!");
-                    return 0;
-            }
-        }
-
-        public static double Add(double num1, double num2)
-        {
-            double result = num1 + num2;
-            return result;
-        }
-
-        public static double Subtr(double num1, double num2)
-        {
-            double result = num1 - num2;
-            return result;
-        }
-
-        public static double Mult(double num1, double num2)
-        {
-            double result = num1 * num2;
-            return result;
-        }
-
-        public static double Div(double num1, double num2)
-        {
-            double result = num1 / num2;
-            return result;
-        }*/
+        
     }
 }
---
+++ CSharp_Basics/Calculator/UI1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class UI1
+    {
+        public static double GetValueFromUser(string message)
+        {
+            double pnum1;
+            while (true)
+            {
+                Console.Write(message);
+                string valueFromUser = Console.ReadLine();
+                if (valueFromUser == "q")
+                {
+                    Console.WriteLine("KONIEC");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                if (!double.TryParse(valueFromUser, out pnum1))
+                { ///Console.WriteLine("Nieliczba! Popraw!");
+                    message = "Nieliczba! Popraw! ";
+                }
+                else return pnum1;
+            }
+        }
+
+        public static string GetOperationFromUser()
+        {
+            
+            Console.Write("Operation: ");
+            string oper = Console.ReadLine();
+            if (oper == "q")
+            {
+                Console.WriteLine("KONIEC");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            return oper;
+        }
+        }
+}
