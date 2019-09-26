0e1ace2ea5c6cec0bffce61266833bbc1efd739d
Pawel <pczesnik@infoprojekt.pl>
2019-09-26 09:00:06 +0200

    Kalkulator - 4 dzialania w metodach

---
+++ CSharp_Basics/Calculator/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 14;
+            double y = 230;
+            double z = 30;
+            double result = x * y;
+            Console.Write("First number=");
+            string num1 = Console.ReadLine();
+            ////int pnum1 = int.Parse(num1);
+            double pnum1 = double.Parse(num1);
+            Console.Write("Second number=");
+            string num2 = Console.ReadLine();
+            ////int pnum2 = int.Parse(num2);
+            double pnum2 = double.Parse(num2);
+            ////result = pnum1 * pnum2;
+            ////Console.WriteLine("x*y="+result);
+            ////Console.WriteLine($"x*y={result}");
+
+            result = Add(pnum1, pnum2);
+            Console.WriteLine($"x+y={result}");
+
+            result = Subtr(pnum1, pnum2);
+            Console.WriteLine($"x-y={result}");
+
+            result = Mult(pnum1, pnum2);
+            Console.WriteLine($"x*y={result}");
+
+
+            result = Div(pnum1, pnum2);
+            Console.WriteLine($"x/y={result}");
+
+            Console.ReadKey();
+
+            
+        }
+
+        public static double Add(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return result;
+        }
+
+        public static double Subtr(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return result;
+        }
+        public static double Mult(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return result;
+        }
+
+        public static double Div(double num1, double num2)
+        {
+            double result = num1 / num2;
+            return result;
+        }
+    }
+}
