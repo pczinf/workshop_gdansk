8d130b1b64e896a1082fbcf8f07a6436fe162d78
Pawel <pczesnik@infoprojekt.pl>
2019-09-26 11:11:53 +0200

    operacje w switchu przeniesione do metody (i jedno pisanie wyniku (bez podania operacji))

--- CSharp_Basics/Calculator/Program.cs
+++ CSharp_Basics/Calculator/Program.cs
@@ -16,20 +16,22 @@ namespace Calculator
              double result = x * y;
              */
             double result;
-            Console.Write("First number=");
+          /*  Console.Write("First number=");
             string num1 = Console.ReadLine();
             ////int pnum1 = int.Parse(num1);
             ////double pnum1 = double.Parse(num1);
             double pnum1;
             if (!double.TryParse(num1, out pnum1))
-            { Console.WriteLine($"Nieliczba! Używamy wartości domyślnej: {pnum1}"); }
-            Console.Write("Second number=");
+            { Console.WriteLine($"Nieliczba! Używamy wartości domyślnej: {pnum1}"); }*/
+            double pnum1 = GetValueFromUser("First number=");
+          /*  Console.Write("Second number=");
             string num2 = Console.ReadLine();
             ////int pnum2 = int.Parse(num2);
             ////double pnum2 = double.Parse(num2);
             double pnum2;
             if (!double.TryParse(num2, out pnum2))
-            { Console.WriteLine($"Nieliczba! Używamy wartości domyślnej: {pnum2}"); }
+            { Console.WriteLine($"Nieliczba! Używamy wartości domyślnej: {pnum2}"); }*/
+            double pnum2 = GetValueFromUser("Second number=");
             ////result = pnum1 * pnum2;
             ////Console.WriteLine("x*y="+result);
             ////Console.WriteLine($"x*y={result}");
@@ -68,6 +70,7 @@ namespace Calculator
             }
             else Console.WriteLine("Nieznana operacja!");
             */
+            /*
             switch (operation)
             {
                 case "+":
@@ -94,8 +97,44 @@ namespace Calculator
 
                 default: Console.WriteLine("Nieznana operacja!");
                     break;
+        }*/
+            result = Calculation(operation, pnum1, pnum2);
+            Console.WriteLine($"Wynik={result}");
+            Console.ReadKey();
         }
-        Console.ReadKey();
+
+        public static double GetValueFromUser(string message)
+        {
+            Console.Write(message);
+            string valueFromUser = Console.ReadLine();
+            double pnum1;
+            if (!double.TryParse(valueFromUser, out pnum1))
+            { Console.WriteLine($"Nieliczba! Używamy wartości domyślnej: {pnum1}"); }
+            return pnum1;
+        }
+
+        public static double Calculation(string oper, double pnum1, double pnum2)
+        {
+            switch (oper)
+            {
+                case "+":
+                    return Add(pnum1, pnum2);
+                case "-":
+                    return Subtr(pnum1, pnum2);
+                case "*":
+                case "x":
+                    return Mult(pnum1, pnum2);
+                case "/":
+                    if (pnum2 == 0)
+                    {
+                        Console.WriteLine("Dzielenie przez zero!");
+                        return 0;
+                    }
+                    return Div(pnum1, pnum2);
+                default:
+                    Console.WriteLine("Nieznana operacja!");
+                    return 0;
+            }
         }
 
         public static double Add(double num1, double num2)
