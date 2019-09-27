c912f143b202c439d04c6e987152fba0f67ebb30
Pawel <pczesnik@infoprojekt.pl>
2019-09-27 13:08:06 +0200

    Testy to str. ContactUs z użyciem metod z klasy

--- CSharp_Basics/SeleniumTests/ContactusTest.cs
+++ CSharp_Basics/SeleniumTests/ContactusTest.cs
@@ -3,12 +3,55 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Threading;
+//using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using SeleniumTests.Pages;
 
 namespace SeleniumTests
 {
     [TestFixture]
-    class ContactusTest
+    class ContactusTest : TestBase
     {
+        ContactUsPage sut;    // deklaracja obiektu dla strony
 
+        [SetUp]
+        public void ContactUssetup()
+        {
+            sut = new ContactUsPage(driver);
+            sut.GoToPage();
+        }
+
+        /*[Test]
+        public void TestIsError()
+        {
+            //driver.Navigate().GoToUrl("http://automationpractice.com/index.php?controller=contact");
+
+            var button = driver.FindElement(By.CssSelector("#submitMessage"));
+            button.Click();
+
+            var alertbar = driver.FindElement(By.CssSelector(".alert p"));
+            var alertbartext = alertbar.Text;
+            string text = "error";
+            StringAssert.Contains(text,alertbartext);
+            Assert.That(alertbartext, Contains.Substring(text));  // wygodniejszy (?) zapis
+
+
+            WebDriverWait waitDriver = new WebDriverWait(driver, new System.TimeSpan(0, 0, 15));
+            var errorMessage = waitDriver.Until(ExpectedConditions.ElementExists(By.CssSelector(".alert-danger li")));
+            
+            Assert.IsTrue(errorMessage.Displayed, "Error message wasn't displayed to user.");
+            Assert.That(errorMessage.Text, Contains.Substring("Invalid email address."));
+        }*/
+
+        [Test]
+        public void TestIsError()
+        {
+            sut.ClickSubmitButton();
+
+            Assert.IsTrue(sut.IsErrorMessageDisplayed(), "Error message wasn't displayed to user.");
+
+            var errorMessage = sut.GetErrorMessageText();
+            Assert.That(errorMessage, Contains.Substring("Invalid email address."));
+        }
     }
 }
---
+++ CSharp_Basics/SeleniumTests/Pages/ContactUsPage.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+//using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests.Pages
+{
+    public class ContactUsPage
+    {
+        private IWebDriver driver;
+        private WebDriverWait waitDriver;
+        private By submitButtonLocator = By.CssSelector("#submitMessage");
+        private By errorMessageLocator = By.CssSelector(".alert-danger li");
+
+        public ContactUsPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            waitDriver = new WebDriverWait(driver, new System.TimeSpan(0, 0, 15));
+        }
+
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php?controller=contact");
+        }
+
+        public void ClickSubmitButton()
+        {
+            driver.FindElement(submitButtonLocator).Click();
+        }
+
+        public string GetErrorMessageText()
+        {
+            var errorMessage = waitDriver.Until(ExpectedConditions.ElementExists(errorMessageLocator));
+            return errorMessage.Text;
+        }
+        public bool IsErrorMessageDisplayed()
+        {
+            var errorMessage = waitDriver.Until(ExpectedConditions.ElementExists(errorMessageLocator));
+            return errorMessage.Displayed;
+        }
+
+    }
+}
