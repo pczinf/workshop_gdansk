53429d5b629e0f9a6feff8350a7e82b46b3dc065
Pawel <pczesnik@infoprojekt.pl>
2019-09-27 08:45:34 +0200

    SeleniumTests dodane; otwarcie, zamknięcie okna Chroma

---
+++ CSharp_Basics/SeleniumTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ede4a28c-836d-4848-8909-9c0cb6ab0006")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharp_Basics/SeleniumTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        [Test]
+        public void Test01()
+        {
+            driver = new ChromeDriver();
+            Assert.AreEqual(2, 1);
+           
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
