ac1ed92adfe1496df37aefd937a10152e5672354
Pawel <pczesnik@infoprojekt.pl>
2019-09-27 10:52:19 +0200

    Testcase z trzema kategoriami

--- CSharp_Basics/SeleniumTests/UnitTest1.cs
+++ CSharp_Basics/SeleniumTests/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace SeleniumTests
         public void Setup()
         {
             driver = new ChromeDriver();
-            var waittime = new System.TimeSpan(0, 0, 1);
+            var waittime = new System.TimeSpan(0, 0, 3);
             driver.Manage().Timeouts().ImplicitWait = waittime;
         }
 
@@ -32,6 +32,63 @@ namespace SeleniumTests
             string text = productcounter.Text;
 
             StringAssert.Contains("5", text);
+        }
+
+        [Test]
+        public void Test02()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+
+            var categoryHeader = driver.FindElement(By.CssSelector("ul.sf-menu > li > a[title='Women']"));
+            categoryHeader.Click();
+            var productcounter = driver.FindElement(By.CssSelector(".heading-counter"));
+            string text = productcounter.Text;
+
+            StringAssert.Contains("7", text);
+        }
+        [Test]
+        public void Test03()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+
+            var categoryHeader = driver.FindElement(By.CssSelector("ul.sf-menu > li > a[title='T-shirts']"));
+            categoryHeader.Click();
+            var productcounter = driver.FindElement(By.CssSelector(".heading-counter"));
+            string text = productcounter.Text;
+
+            StringAssert.Contains("1", text);
+        }
+
+        [Test]
+        public void Test01a()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+
+            var categoryHeader = driver.FindElement(By.CssSelector("ul.sf-menu > li > a[title='Dresses']"));
+            categoryHeader.Click();
+           
+            var productcounter = driver.FindElement(By.CssSelector(".heading-counter"));
+            string text = productcounter.Text;
+            var webElements = driver.FindElements(By.CssSelector("div.product-container"));
+            string actualElementCount = webElements.Count.ToString();
+            StringAssert.Contains(actualElementCount, text);
+        }
+
+        [TestCase("Dresses")]
+        [TestCase("Women")]
+        [TestCase("T-shirts")]
+        public void Test123(string categ)
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+
+            var categoryHeader = driver.FindElement(By.CssSelector("ul.sf-menu > li > a[title='"+ categ+"']"));
+            categoryHeader.Click();
+
+            var productcounter = driver.FindElement(By.CssSelector(".heading-counter"));
+            string text = productcounter.Text;
+            var webElements = driver.FindElements(By.CssSelector("div.product-container"));
+            string actualElementCount = webElements.Count.ToString();
+            StringAssert.Contains(actualElementCount, text);
         }
 
         [TearDown]
